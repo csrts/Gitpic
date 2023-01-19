6d5f3edb2ec803ed9686c98f6b60d7fb9ee569b2
csrts <64183095+csrts@users.noreply.github.com>
2023-01-19 16:42:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
+from lxml import html
 import requests
-from bs4 import BeautifulSoup
 import schedule
 import time
-import telegram
+from aiogram.bot import Bot
+from aiogram.types import Message
 
-bot = telegram.Bot(token='YOUR_TELEGRAM_BOT_TOKEN')
+bot = Bot(token='YOUR_TELEGRAM_BOT_TOKEN')
 
-def job():
+async def job():
     # 爬取网页源码
     url = "https://tool.lu/todayonhistory/"
     page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
+    tree = html.fromstring(page.content)
 
-    # 取出部分内容
-    text = soup.find_all("li").get_text()
+    # 取出所有<li>标签中的内容
+    tohlis = tree.xpath('//li')
 
-    # 创建搜索 URL
-    search_url = "http://www.google.com/search?q="
-    query = text.split(" ")[-1]
-    url = search_url + query
+    # 遍历所有<li>标签
+    for li in tohlis:
+        text = li.text_content()
 
-    # 发送消息
-    bot.send_message(chat_id='YOUR_CHAT_ID', text=text + ' ' + url)
+        # 创建搜索 URL
+        search_url = "http://www.google.com/search?q="
+        query = text.split(" ")[-1]
+        url = search_url + query
+
+        # 发送消息
+        await bot.send_message(chat_id='YOUR_CHAT_ID', text=text + ' ' + url)
 
 # 每天8点运行
 schedule.every().day.at("8:00").do(job)
